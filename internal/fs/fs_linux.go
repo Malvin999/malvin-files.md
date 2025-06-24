16e709203527bfe2811f5ef7298787832c7281a2
Artem Zakirullin <artemzr@gmail.com>
2025-06-24 17:02:30 +0300

    fix switch to microseconds

--- internal/fs/fs_linux.go
+++ internal/fs/fs_linux.go
@@ -10,5 +10,5 @@ import (
 var Ctime = func(fi os.FileInfo) int64 {
 	stat := fi.Sys().(*syscall.Stat_t)
 
-	return stat.Ctim.Sec*1_000_000_000 + stat.Ctim.Nsec
+	return (stat.Ctim.Sec*1_000_000_000 + stat.Ctim.Nsec) / 1000
 }
