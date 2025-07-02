84463763a9e1cbe12c09239fe1758e735e172775
Artem Zakirullin <artemzr@gmail.com>
2025-07-02 15:21:23 +0300

    fix change groups

--- web/app.js
+++ web/app.js
@@ -429,8 +429,8 @@ function renderSidebar(focusDir = '') {
 
 
     const groups = [
-        ['_read_', '_watch_', '_shop_'],
         ['journal', 'habits', 'insights', 'archive'],
+        ['_read_', '_watch_', '_shop_'],
         ['today', 'later']
     ];
 
